d96fc7825b6edded5ff006d23cf900bfd61446b1
samuel <damosisamuel@gmail.com>
2023-02-13 10:54:59 +0100

    index + styles

---
+++ Artificial/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Base01</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+<body>
+    <!-- header starts here -->
+    <!-- <header>
+        <div class="row">
+            <div class="col-4">
+                <p>Welcome</p>
+
+            </div>
+           
+         
+
+        </div>
+
+    </header> -->
+    <!-- header ends here -->
+    <!-- main starts here -->
+    <main class="">
+        <section class="sect1">
+            <div class="row">
+                <div class="offset-2 col-5">
+                    <div>
+                        <img style="height:178px; width:178px;" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/9d37c6553465529cafa82304/circlegradient2.png" alt="">
+                        <h1 class="myfirst">Humans for the future</h1>
+                        <p class="gty">Lorem ipsum dolor sit amet, consectetur adipiscing elit nullam nunc justo sagittis suscipit ultrices.</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+        <section class="sect2">
+            <div class="row ">
+                <div class="offset-2 col-6 pt-4 ">
+                    <div class="shrek">
+                        <h5>TECHNOLOGY NEWS</h5>
+
+                        <h2>Human-robot</h2>
+                        <p class="tit "></p>
+
+
+                        <p class="inhuman">In human-robot collaboration, the robot assists the human operator. This means: The machine does not replace the human, but complements his capabilities and relieves him of arduous tasks. These can include overhead work, for example, or the lifting of heavy loads. Autonomous, collaborative robots are also used to supply production workstations</p>
+
+                        <button class="mybtn">LEARN MORE</button>
+                    </div>
+
+                </div>
+                <div class="col-4 ">
+                    <img class="mylog" src="https://images01.nicepage.com/c461c07a441a5d220e8feb1a/78325435de7e5b8f82cfa241/bubbles2.png" alt="">
+
+                </div>
+
+            </div>
+
+        </section>
+
+
+    </main>
+    <!-- main ends here -->
+    <!-- footer starts here -->
+    <footer>
+
+    </footer>
+    <!-- footer ends here -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    
+</body>
+</html>
